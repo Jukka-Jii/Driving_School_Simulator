d3b4c7bc98a8ab7fdaed1f0e20f401763162b585
VillePellosniemi <ville.pellosniemi@metropolia.fi>
2020-05-04 17:35:12 +0300

    Brakes added

--- Assets/Scripts/SimpleCarController.cs
+++ Assets/Scripts/SimpleCarController.cs
@@ -1,13 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleCarController : MonoBehaviour
 {
 
+	private float m_horizontalInput;
+	private float m_verticalInput;
+	private float m_steeringAngle;
+	private Rigidbody rigidbody;
+	public float KPH;
+
+	public Text speedLimit;
+
+	public WheelCollider frontDriverW, frontPassengerW;
+	public WheelCollider rearDriverW, rearPassengerW;
+	public Transform frontDriverT, frontPassengerT;
+	public Transform rearDriverT, rearPassengerT;
+	public float maxSteerAngle = 30;
+	public float motorForce = 50;
+
+	public bool isBrake = false;
+	public float brakeTorque = 15000f;
+
 	public void Start()
 	{
 		getObjects();
+		speedLimit.text = "";
 	}
 
 	public void GetInput()
@@ -28,6 +48,34 @@ public class SimpleCarController : MonoBehaviour
 		frontDriverW.motorTorque = m_verticalInput * motorForce;
 		frontPassengerW.motorTorque = m_verticalInput * motorForce;
 		KPH = rigidbody.velocity.magnitude * 3.6f;
+	}
+
+	void HandBrake ()
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			isBrake = true;
+		} else
+		{
+			isBrake = false;
+		}
+		if (isBrake == true)
+		{
+			rearDriverW.brakeTorque = brakeTorque;
+			rearPassengerW.brakeTorque = brakeTorque;
+			frontDriverW.brakeTorque = brakeTorque;
+			frontPassengerW.brakeTorque = brakeTorque;
+			rearDriverW.motorTorque = 0;
+			rearPassengerW.motorTorque = 0;
+			frontDriverW.motorTorque = 0;
+			frontPassengerW.motorTorque = 0;
+		} else
+		{
+			rearDriverW.brakeTorque = 0;
+			rearPassengerW.brakeTorque = 0;
+			frontDriverW.brakeTorque = 0;
+			frontPassengerW.brakeTorque = 0;
+		}
 	}
 
 	private void UpdateWheelPoses()
@@ -55,6 +103,8 @@ public class SimpleCarController : MonoBehaviour
 		Steer();
 		Accelerate();
 		UpdateWheelPoses();
+		ExceedingSpeedLimit();
+		HandBrake();
 	}
 
 	private void getObjects()
@@ -62,16 +112,15 @@ public class SimpleCarController : MonoBehaviour
 		rigidbody = GetComponent<Rigidbody>();
 	}
 
-	private float m_horizontalInput;
-	private float m_verticalInput;
-	private float m_steeringAngle;
-	private Rigidbody rigidbody;
-	public float KPH;
-
-	public WheelCollider frontDriverW, frontPassengerW;
-	public WheelCollider rearDriverW, rearPassengerW;
-	public Transform frontDriverT, frontPassengerT;
-	public Transform rearDriverT, rearPassengerT;
-	public float maxSteerAngle = 30;
-	public float motorForce = 50;
+	void ExceedingSpeedLimit ()
+	{
+		if (KPH > 40)
+		{
+			speedLimit.text = "Speed Limit Exceeded!";
+			Debug.Log(speedLimit);
+		} else
+		{
+			speedLimit.text = "";
+		}
+	}
 }
